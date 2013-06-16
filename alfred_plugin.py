0716475f60f10756c4af291cfa914848724cfb19
kun <huangzhongkun@douban.com>
2013-06-16 17:25:31 +0800

    -a
    add delicious python  api class

---
+++ alfred_plugin.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+from feedback import Feedback
+from deliciousapi import DeliciousAPI
+import sys
+import re
+import urllib2
+import urllib
+
+query = '{query}'
+d = DeliciousAPI()
+
+user = d.get_user('kun77416', None, query)
+fb = Feedback()
+
+i = 0
+try:
+    for b in user.bookmarks:
+        print b
+        print '\n'
+        fb.add_item(b[2], subtitle=b[2], uid=i, arg=b[0])
+
+except SyntaxError as e:
+    if ('EOF', 'EOL' in e.msg):
+        fb.add_item('...')
+    else:
+        fb.add_item('SyntaxError', e.msg)
+except Exception as e:
+        fb.add_item(e.__class__.__name__,
+            subtitle=e.message) 
+
+print fb
